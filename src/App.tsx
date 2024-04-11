6770daa727a7239fc1a266a02eb1682648dc2e0c
Dvwhite81 <dvwhite1981@gmail.com>
2024-04-11 13:29:40 -0400

    Done for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,126 @@
-import Logo2 from './assets/images/Logo-Circle.png';
-import Logo from '/Logo-Circle.png';
-import './App.css';
+import { useMemo } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import { Container } from 'react-bootstrap';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import { useLocalStorage } from './utils/useLocalStorage';
+import { NoteData, RawNote, Tag } from './utils/types';
+import { NoteList } from './components/NoteList';
+import { NewNote } from './components/NewNote';
+import { NoteLayout } from './components/NoteLayout';
+import { Note } from './components/Note';
+import { EditNote } from './components/EditNote';
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', []);
+  const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  const handleCreateNote = ({ tags, ...data }: NoteData) => {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        {
+          ...data,
+          id: uuidV4(),
+          tagIds: tags.map((tag) => tag.id),
+        },
+      ];
+    });
+  };
+
+  const handleUpdateNote = (id: string, { tags, ...data }: NoteData) => {
+    setNotes((prevNotes) => {
+      return prevNotes.map((note) => {
+        if (note.id === id) {
+          return {
+            ...note,
+            ...data,
+            tagIds: tags.map((tag) => tag.id),
+          };
+        } else {
+          return note;
+        }
+      });
+    });
+  };
+
+  const handleDeleteNote = (id: string) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note) => note.id !== id);
+    });
+  };
+
+  const addTag = (tag: Tag) => {
+    setTags((prevTags) => [...prevTags, tag]);
+  };
+
+  const updateTag = (id: string, label: string) => {
+    setTags((prevTags) => {
+      return prevTags.map((tag) => {
+        if (tag.id === id) {
+          return { ...tag, label };
+        } else {
+          return tag;
+        }
+      });
+    });
+  };
+
+  const deleteTag = (id: string) => {
+    setTags((prevTags) => {
+      return prevTags.filter((tag) => tag.id !== id);
+    });
+  };
+
   return (
-    <>
-      <h1>Hello</h1>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src={Logo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={Logo2} className="logo react" alt="React logo" />
-      </a>
-    </>
+    <Container className="my-4">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <NoteList
+              notes={notesWithTags}
+              availableTags={tags}
+              updateTag={updateTag}
+              deleteTag={deleteTag}
+            />
+          }
+        />
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={handleCreateNote}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        />
+        <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
+          <Route index element={<Note onDelete={handleDeleteNote} />} />
+          <Route
+            path="edit"
+            element={
+              <EditNote
+                onSubmit={handleUpdateNote}
+                onAddTag={addTag}
+                availableTags={tags}
+              />
+            }
+          />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   );
 }
 
